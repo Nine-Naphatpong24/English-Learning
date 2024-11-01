0350a920c906bd59c9cda577a806c3d890c8f469
Nine-Naphatpong24 <ninezazatew@gmail.com>
2024-11-01 21:40:46 +0700

    second commit

---
+++ englishlearning/lib/HomeAppBar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(25),
+      child: const Row(
+        children: [
+          Icon(Icons.sort, size: 30, color: Color(0xFF4C53A5)),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+            ),
+            child: Text(
+              "English Learning",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4C53A5)
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
